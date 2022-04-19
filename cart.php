aa54e31f1efe5e0b889c6dc2107b4a0853bc54d7
ghartemann <g.hartemann@gmail.com>
2022-04-19 13:18:13 +0200

    finished quest I guess

--- cart.php
+++ cart.php
@@ -1,7 +1,34 @@
-<?php require 'inc/head.php'; ?>
+<?php
+
+session_start();
+require 'inc/head.php';
+require 'inc/data/products.php';
+
+// On teste si la variable de session existe et contient une valeur
+if (empty($_SESSION['login'])) {
+    header('Location: login.php');
+    exit();
+}
+
+$selectedCookies[] = $catalog[$_SESSION['cookie_id']];
+?>
+
+<p>Cookies sélectionnés</p>
 <section class="cookies container-fluid">
     <div class="row">
-        TODO : Display shopping cart items from $_SESSION here.
+        <ul>
+            <?php
+            if (isset($_SESSION['cookie_id'])) {
+                foreach ($selectedCookies as $selectedCookie) { ?>
+                    <li>
+                        <p><?= $selectedCookie['name'] ?></p>
+                        <img src="assets/img/product-<?= $_SESSION['cookie_id'] ?>.jpg">
+                    </li>
+
+            <?php }
+            }
+            ?>
+        </ul>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php require 'inc/data/products.php'; ?>
-<?php require 'inc/head.php'; ?>
+<?php
+
+session_start();
+require 'inc/data/products.php';
+require 'inc/head.php';
+
+if (isset($_GET['id'])) {
+    $_SESSION['cookie_id'] = $_GET['id'];
+}
+
+?>
+
 <section class="cookies container-fluid">
     <div class="row">
         <?php foreach ($catalog as $id => $cookie) { ?>
@@ -9,7 +19,7 @@
                     <figcaption class="caption">
                         <h3><?= $cookie['name']; ?></h3>
                         <p><?= $cookie['description']; ?></p>
-                        <a href="?add_to_cart=<?= $id; ?>" class="btn btn-primary">
+                        <a href="?id=<?= $id ?>" class="btn btn-primary">
                             <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
                         </a>
                     </figcaption>
